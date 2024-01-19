d02a6e758481c9cdcaca97fb90bf0ae88c33bf78
Bussler <maarten.bussler@gmail.com>
2024-01-19 23:19:18 +0100

    - add layer collision matrix - put bullets to collision

--- Assets/Scripts/Bullets/BasicBullet.cs
+++ Assets/Scripts/Bullets/BasicBullet.cs
@@ -21,13 +21,14 @@ public class BasicBullet : MonoBehaviour
         _rb.velocity = transform.up * speed;
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnCollisionEnter2D(Collision2D other)
     {
         GameObject hitObject = other.gameObject;
         
-        if (hitObject.CompareTag("Bullet") || hitObject.CompareTag("EnemyBullet")) // Ignore other bullets TODO: Do with collision layer
+        if (hitObject.CompareTag("Bullet") || hitObject.CompareTag("EnemyBullet"))
         {
-            return;
+            // if bullet hits another bullet, destroy both
+            ObjectPoolManager.Instance.DespawnObject(other.gameObject);
         }
         else if (hitObject.CompareTag("Player"))
         {
