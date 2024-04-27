6edec60fcf91456669fbe1857abb3648e645ff54
Bussler <maarten.bussler@gmail.com>
2024-04-27 11:49:01 +0200

    add inference checkpoints and images

--- python_reinforcement_learning/python_reinforcement_learning/inference/rllib_inference.py
+++ python_reinforcement_learning/python_reinforcement_learning/inference/rllib_inference.py
@@ -26,6 +26,7 @@ def inference_rllib(env: HunterEnvironment, model_path: str, num_episodes=10):
     while not terminated and not truncated:
         action = algo.compute_single_action(obs)
         obs, reward, terminated, truncated, info = env.step(action)
+        env.render()
         episode_reward += reward
 
     env.close()
@@ -34,10 +35,12 @@ def inference_rllib(env: HunterEnvironment, model_path: str, num_episodes=10):
 
 
 if __name__ == "__main__":
-    sim_config = MockSimulationConfig(number_enemies=4, field_size=20, enemy_live_for_steps=20)
-    render_config = RendererConfig(window_size=512, render_fps=4, render_mode=RendererMode.Human, store_dir="images")
+    sim_config = MockSimulationConfig(number_enemies=4, field_size=20, enemy_live_for_steps=40)
+    render_config = RendererConfig(
+        window_size=512, render_fps=4, render_mode=RendererMode.RGBArray, store_dir="images_inference"
+    )
     env_config = HunterEnvironmentConfig(
         size=20, max_timestep=1000, udp_address=None, simulation_config=sim_config, render_config=render_config
     )
     env = HunterEnvironment(env_config)
-    inference_rllib(env, "path/to/model/checkpoint")
+    inference_rllib(env, "C:/unityProjects/ProjectHunters/python_reinforcement_learning/rllib_checkpoints")
